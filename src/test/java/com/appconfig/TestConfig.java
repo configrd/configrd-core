f58e97a06c416440199fc4d9efba9b3a7cfe93d0
kkarski <kkarski@appcrossings.com>
2015-10-06 15:51:31 -0700

    added test for host override of environment

--- src/test/java/com/appconfig/TestConfig.java
+++ src/test/java/com/appconfig/TestConfig.java
@@ -7,7 +7,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -17,6 +19,16 @@ public class TestConfig {
 	private InetAddress inet;
 	private String host = "http://static.ca.pixtulate.com";
 	private ConfigImpl config;
+
+	@BeforeClass
+	public void env() {
+		System.setProperty("env", "QA");
+	}
+
+	@AfterClass
+	public void destroyEnv() {
+		System.setProperty("env", "");
+	}
 
 	@BeforeMethod
 	public void setUp() throws Exception {
