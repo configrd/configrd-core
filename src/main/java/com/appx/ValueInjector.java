4d04b949c87915076dd9263f71a859c199a43bd3
kkarski <kkarski@appcrossings.com>
2015-12-14 21:32:08 -0800

    commented out change listener due to shippable compile issues

--- src/main/java/com/appx/ValueInjector.java
+++ src/main/java/com/appx/ValueInjector.java
@@ -26,40 +26,38 @@ public class ValueInjector implements ConfigChangeListener {
   @Override
   public void propertyChanged(String key, Object value) {
 
-    Map<String, Object> beans = context.getBeansWithAnnotation(Value.class);
-
-    for (Object o : beans.values()) {
-
-      Field[] values = o.getClass().getFields();
-
-      for (Field f : values) {
-        if (f.isAnnotationPresent(Value.class)
-            && f.getAnnotation(Value.class).value().equalsIgnoreCase(key)) {
-          try {
-            f.set(o, value);
-          } catch (Exception e) {
-            // nothing
-          }
-
-        }
-      }
-
-      Method[] methods = o.getClass().getMethods();
-
-      for (Method f : methods) {
-        if (f.isAnnotationPresent(Value.class)
-            && f.getAnnotation(Value.class).value().equalsIgnoreCase(key)
-            && f.getParameterCount() == 1) {
-          try {
-            f.invoke(o, value);
-          } catch (Exception e) {
-
-          }
-        }
-      }
-
-    }
-
+//    Map<String, Object> beans = context.getBeansWithAnnotation(Value.class);
+//
+//    for (Object o : beans.values()) {
+//
+//      Field[] values = o.getClass().getFields();
+//
+//      for (Field f : values) {
+//        if (f.isAnnotationPresent(Value.class)
+//            && f.getAnnotation(Value.class).value().equalsIgnoreCase(key)) {
+//          try {
+//            f.set(o, value);
+//          } catch (Exception e) {
+//            // nothing
+//          }
+//
+//        }
+//      }
+//
+//      Method[] methods = o.getClass().getMethods();
+//
+//      for (Method f : methods) {
+//        if (f.isAnnotationPresent(Value.class)
+//            && f.getAnnotation(Value.class).value().equalsIgnoreCase(key)
+//            && f.getParameterCount() == 1) {
+//          try {
+//            f.invoke(o, value);
+//          } catch (Exception e) {
+//
+//          }
+//        }
+//      }
+//    }
   }
 
 }
